f97df1594f0cab15abc0bc71cc66c05b83fd42b0
MaceSAIT <166629082+MaceSAIT@users.noreply.github.com>
2025-02-11 10:53:45 -0700

    added font and image, need to implement image

--- app/page.js
+++ app/page.js
@@ -3,26 +3,9 @@ import Link from 'next/link'
 export default function Home() {
     return (
       <main>
-        <div>
-            <h1 className="text-3xl font-bold m-6 text-center">Mace's CPRG 306 Assignments</h1>
-            <h2>Home</h2>
-            <br></br>
-            <br></br>
-
-            <ul>
-              <li>
-                <Link href={{pathname:'/week-2'}}>Week 2 Page</Link>
-              </li>
-              <li>
-              <Link href={{pathname:'/week-3'}}>Week 3 Page</Link>
-              </li>
-              <li>
-              <Link href={{pathname:'/week-4'}}>Week 4 Page</Link>
-              </li>
-              <li>
-              <Link href={{pathname:'/week-5'}}>Week 5 Page</Link>
-              </li>
-            </ul>
+        <div className='flex flex-col justify-center align-middle'>
+          <img src="./vanguard-cyber.png"></img>
+          <h1 className="text-3xl m-6 text-center font-(vanguardFont)">We'll be with you shortly.</h1>
         </div>
         
       </main>
