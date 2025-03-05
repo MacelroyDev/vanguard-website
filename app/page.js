22f836bc9717f8230d908bd917e2b8c8c6560436
MacelroyDev <macehowald@gmail.com>
2025-03-05 12:40:40 -0700

    fixed image and moved code to seperate file

--- app/page.js
+++ app/page.js
@@ -1,16 +1,12 @@
-import Link from 'next/link'
+import Soon from './beta/soon'
 
 export default function Home() {
 
-  const imageURL = "https://cdn.discordapp.com/attachments/356543230809997324/1334960971739041852/Picsart_25-01-31_10-57-25-171.png?ex=67ae40ca&is=67acef4a&hm=cae6807168e42edb46af1f4e218bf1322251304ac9042a5640389f303724a646&"
+  const imageURL = ''
 
     return (
       <main>
-        <div className='flex flex-col text-center'>
-          <img className='mx-auto drop-shadow-vanguard-shadow' src={imageURL} width='25%' height='25%'></img>
-          <h1 className="text-3xl m-6 font-(vanguardFont) drop-shadow-xl">We'll be with you soon.</h1>
-        </div>
-        
+        <Soon/>
       </main>
     );
   }
